24a0f0c04d4f5f3827b77b6149bb1b6040cfcb74
kevinxu <kevin671xu@gmail.com>
2024-07-20 18:03:47 +0900

    add

--- models/hypernetworks.py
+++ models/hypernetworks.py
@@ -2,9 +2,113 @@ import torch
 import torch.nn as nn
 
 
-class LoRAHyperNetwork(nn.Module):
+class LoRAHyperNetwork(nn.Linear, LoRALayer):
+    # LoRA implemented in a dense layer
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        r: int = 0,
+        lora_alpha: int = 1,
+        lora_dropout: float = 0.0,
+        enable_lora: List[bool] = [False],
+        fan_in_fan_out: bool = False,
+        merge_weights: bool = True,
+        **kwargs
+    ):
+        nn.Linear.__init__(self, in_features, out_features, **kwargs)
+        LoRALayer.__init__(
+            self,
+            r=r,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            merge_weights=merge_weights,
+        )
+        assert (
+            out_features % len(enable_lora) == 0
+        ), "The length of enable_lora must divide out_features"
+        self.enable_lora = enable_lora
+        self.fan_in_fan_out = fan_in_fan_out
+        # Actual trainable parameters
+        if r > 0 and any(enable_lora):
+            self.lora_A = nn.Parameter(
+                self.weight.new_zeros((r * sum(enable_lora), in_features))
+            )
+            self.lora_B = nn.Parameter(
+                self.weight.new_zeros(
+                    (out_features // len(enable_lora) * sum(enable_lora), r)
+                )
+            )  # weights for Conv1D with groups=sum(enable_lora)
+            self.scaling = self.lora_alpha / self.r
+            # Freezing the pre-trained weight matrix
+            self.weight.requires_grad = False
+            # Compute the indices
+            self.lora_ind = self.weight.new_zeros(
+                (out_features,), dtype=torch.bool
+            ).view(len(enable_lora), -1)
+            self.lora_ind[enable_lora, :] = True
+            self.lora_ind = self.lora_ind.view(-1)
+        self.reset_parameters()
+        if fan_in_fan_out:
+            self.weight.data = self.weight.data.transpose(0, 1)
+
+    def reset_parameters(self):
+        nn.Linear.reset_parameters(self)
+        if hasattr(self, "lora_A"):
+            # initialize A the same way as the default for nn.Linear and B to zero
+            nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+            nn.init.zeros_(self.lora_B)
+
+    def zero_pad(self, x):
+        result = x.new_zeros((len(self.lora_ind), *x.shape[1:]))
+        result[self.lora_ind] = x
+        return result
+
+    def merge_AB(self):
+        def T(w):
+            return w.transpose(0, 1) if self.fan_in_fan_out else w
+
+        delta_w = F.conv1d(
+            self.lora_A.unsqueeze(0),
+            self.lora_B.unsqueeze(-1),
+            groups=sum(self.enable_lora),
+        ).squeeze(0)
+        return T(self.zero_pad(delta_w))
+
+    def train(self, mode: bool = True):
+        def T(w):
+            return w.transpose(0, 1) if self.fan_in_fan_out else w
+
+        nn.Linear.train(self, mode)
+        if mode:
+            if self.merge_weights and self.merged:
+                # Make sure that the weights are not merged
+                if self.r > 0 and any(self.enable_lora):
+                    self.weight.data -= self.merge_AB() * self.scaling
+                self.merged = False
+        else:
+            if self.merge_weights and not self.merged:
+                # Merge the weights and mark it
+                if self.r > 0 and any(self.enable_lora):
+                    self.weight.data += self.merge_AB() * self.scaling
+                self.merged = True
+
+    def forward(self, x: torch.Tensor):
+        def T(w):
+            return w.transpose(0, 1) if self.fan_in_fan_out else w
+
+        if self.merged:
+            return F.linear(x, T(self.weight), bias=self.bias)
+        else:
+            result = F.linear(x, T(self.weight), bias=self.bias)
+            if self.r > 0:
+                result += self.lora_dropout(x) @ T(self.merge_AB().T) * self.scaling
+            return result
+
+
+class HyperNetwork(nn.Module):
     def __init__(self, h_dim: int, rank: int, t_dim: int):
-        super(LoRAHyperNetwork, self).__init__()
+        super(HyperNetwork, self).__init__()
 
         self.wa = nn.Parameter(torch.Tensor(rank, h_dim, t_dim))
         self.ba = nn.Parameter(torch.Tensor(rank, h_dim))
@@ -16,3 +120,57 @@ class LoRAHyperNetwork(nn.Module):
         lora_a = torch.einsum("rht,ht->rh", self.wa, temb) + self.ba
         lora_b = torch.einsum("hrh,rht->ht", self.wb, temb) + self.bb
         return lora_a, lora_b
+
+
+class Attention(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _attn(self, x, merge_weights):
+        pass
+
+
+class MLP(nn.Module):
+    pass
+
+
+class TimeEmbedding(nn.Module):
+    pass
+
+
+class PrimaryNetwork(nn.Module):
+    def __init__(self, t_dim):
+        super().__init__()
+        self.mlp = MLP()
+        self.attn = Attention()
+
+        self.hyper = HyperNetwork(h_dim=128, rank=32, t_dim=t_dim)
+
+    def forward(self, x, n_loops):
+        x = self.mlp(x)
+
+        for t in range(n_loops):
+            t_emb = TimeEmbedding(t)
+            merge_weights = self.hyper(t_emb)
+            x += self.attn(x, merge_weights)
+
+        return x
+
+
+class LoRAPrimaryNetwork(nn.Module):
+    def __init__(self, t_dim):
+        super().__init__()
+        self.mlp = MLP()
+        self.attn = Attention()
+
+        self.hyper = LoRAHyperNetwork(h_dim=128, rank=32, t_dim=t_dim)
+
+    def forward(self, x, n_loops):
+        x = self.mlp(x)
+
+        for t in range(n_loops):
+            t_emb = TimeEmbedding(t)
+            lora_a, lora_b = self.hyper(t_emb)
+            x += self.attn(x, merge_weights)
+
+        return x
