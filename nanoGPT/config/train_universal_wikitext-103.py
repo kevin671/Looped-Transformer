d5719099706ff2f14fbe562a8d77f6c975c410ec
kevinxu <kevin671xu@gmail.com>
2024-07-31 14:46:54 +0900

    add Universal Transformer

--- nanoGPT/config/train_universal_wikitext-103.py
+++ nanoGPT/config/train_universal_wikitext-103.py
@@ -1,24 +1,25 @@
 # config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+import uuid
 
-init_from = "dynamic_looped_gpt"
+init_from = "universal_gpt"
 
-# model config
-n_loops = 100
 n_layer = 1
 n_head = 12
 n_embd = 768
 
+unique_suffix = uuid.uuid4().hex[:4]  # ランダムな8文字の文字列を生成
+
 wandb_log = True
 wandb_project = "perplexity"
-wandb_run_name = f"dynamic_looped_{n_loops}_loop_wikitext-103"
+wandb_run_name = f"universal_{n_layer}_layer_wikitext-103_{unique_suffix}"
 
-out_dir = f"out/dynamic_looped_{n_loops}_loop_wikitext-103"
+out_dir = f"out/universal_{n_layer}_layer_wikitext-103_{unique_suffix}"
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 2
+batch_size = 6
 block_size = 1024
 gradient_accumulation_steps = 10 * 4
 
